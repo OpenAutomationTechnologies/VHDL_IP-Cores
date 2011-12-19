24d888c0a40e61cb6596dc0554da14ae56aedbbd
Thomas Mair <thomas.mair@br-automation.com>
2011-12-19 13:46:04 +0100

    changed the release scripts

--- altera_release.bat
+++ altera_release.bat
@@ -1,16 +1,21 @@
 @echo off
+mkdir release\altera_sopc\POWERLINK\src
+mkdir release\altera_sopc\POWERLINK\src\lib 
+mkdir release\altera_sopc\POWERLINK\src\openMAC_DMAmaster
+mkdir release\altera_sopc\POWERLINK\sdc
+mkdir release\altera_sopc\POWERLINK\doc
 
-copy active_hdl\src\*.vhd				..\release\altera_sopc\POWERLINK\src
-copy active_hdl\src\lib\*.vhd				..\release\altera_sopc\POWERLINK\src\lib
-copy active_hdl\src\openMAC_DMAmaster\*.vhd		..\release\altera_sopc\POWERLINK\src\openMAC_DMAmaster
-copy active_hdl\compile\*.vhd				..\release\altera_sopc\POWERLINK\src
-copy active_hdl\src\altera_sopc\powerlink_hw.tcl	..\release\altera_sopc\POWERLINK
-copy active_hdl\src\altera_sopc\*.sdc			..\release\altera_sopc\POWERLINK\sdc
-copy documentation\*.pdf				..\release\altera_sopc\POWERLINK\doc
+copy active_hdl\src\*.vhd				release\altera_sopc\POWERLINK\src
+copy active_hdl\src\lib\*.vhd				release\altera_sopc\POWERLINK\src\lib
+copy active_hdl\src\openMAC_DMAmaster\*.vhd		release\altera_sopc\POWERLINK\src\openMAC_DMAmaster
+copy active_hdl\compile\*.vhd				release\altera_sopc\POWERLINK\src
+copy active_hdl\src\altera_sopc\powerlink_hw.tcl	release\altera_sopc\POWERLINK
+copy active_hdl\src\altera_sopc\*.sdc			release\altera_sopc\POWERLINK\sdc
+copy documentation\*.pdf				release\altera_sopc\POWERLINK\doc
 
-del ..\release\altera_sopc\POWERLINK\src\*_TB.vhd
-del ..\release\altera_sopc\POWERLINK\src\*_Xilinx.vhd
-del ..\release\altera_sopc\POWERLINK\src\openMAC_DMAmaster\plb_master_handler.vhd
-del ..\release\altera_sopc\POWERLINK\src\plb_powerlink.vhd
+del release\altera_sopc\POWERLINK\src\*_TB.vhd
+del release\altera_sopc\POWERLINK\src\*_Xilinx.vhd
+del release\altera_sopc\POWERLINK\src\openMAC_DMAmaster\plb_master_handler.vhd
+del release\altera_sopc\POWERLINK\src\plb_powerlink.vhd
 
 @echo on
