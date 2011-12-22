e2fc3d5ccb9e0575b74cc6a208e3fb2b2f79bc70
Jörg Zelenka <joerg.zelenka@br-automation.com>
2011-12-22 07:52:46 +0100

    added memory initialization with hex files

--- active_hdl/src/OpenMAC_DPR_Altera.vhd
+++ active_hdl/src/OpenMAC_DPR_Altera.vhd
@@ -37,6 +37,8 @@
 ------------------------------------------------------------------------------------------------------------------------      
 --  2009-08-07  V0.01		Converted to official version.
 --	2010-05-03	V0.02		added packet buffer dpr
+--  2011-12-22	V0.03		added initialization files
+--							removed dpr_8_8
 ------------------------------------------------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------
@@ -72,7 +74,7 @@ architecture struct of Dpr_16_16 is
 begin
 
 Ram: COMPONENT altsyncram
-		GENERIC MAP ( OPERATION_MODE => "BIDIR_DUAL_PORT",
+		GENERIC MAP ( OPERATION_MODE => "BIDIR_DUAL_PORT", INIT_FILE => "dpr_16_16.hex",
 					  WIDTH_A => 16, WIDTHAD_A => 8, NUMWORDS_A => 256, WIDTH_BYTEENA_A => 2,
 					  WIDTH_B => 16, WIDTHAD_B => 8, NUMWORDS_B => 256, WIDTH_BYTEENA_B => 2
 					 )
@@ -118,7 +120,7 @@ architecture struct of Dpr_16_32 is
 begin
 
 Ram: COMPONENT altsyncram
-		GENERIC MAP ( OPERATION_MODE => "DUAL_PORT", --INIT_FILE => "..\src\Null_alt.hex",
+		GENERIC MAP ( OPERATION_MODE => "DUAL_PORT", INIT_FILE => "dpr_32_16.hex",
 					  WIDTH_A => 16, WIDTHAD_A => 8, NUMWORDS_A => 256, WIDTH_BYTEENA_A => 2,
 					  WIDTH_B => 32, WIDTHAD_B => 7, NUMWORDS_B => 128
 					 )
@@ -130,50 +132,6 @@ Ram: COMPONENT altsyncram
 			address_a => AddrA,		address_b => AddrB,					
 			data_a    => DiA,
 									q_b       => DoB
-			);
-
-end struct;
-
--------------------------------------------------------------------------------
--- 8 / 8 DPR 
---
-LIBRARY ieee;                   
-USE ieee.std_logic_1164.all;    
-USE ieee.std_logic_arith.all;   
-USE ieee.std_logic_unsigned.all;
-LIBRARY altera_mf;
-USE altera_mf.altera_mf_components.all;
-LIBRARY lpm;
-USE lpm.lpm_components.all;
-
-entity Dpr_8_8 is
-  generic(Simulate	:  in	boolean);
-  port (
-	 ClkA,  ClkB		:  in  std_logic;
-	 WeA,   WeB			:  in  std_logic := '0';
-	 EnA,   EnB			:  in  std_logic := '1';
-	 AddrA, AddrB		:  in  std_logic_vector ( 8 downto 0);
-	 DiA,   DiB			:  in  std_logic_vector ( 7 downto 0) := (others => '0');
-	 DoA,   DoB			:  out  std_logic_vector( 7 downto 0) 
-	 );
-end Dpr_8_8;
-
-architecture struct of Dpr_8_8 is
-begin
-
-Ram: COMPONENT altsyncram
-		GENERIC MAP (	operation_mode => "BIDIR_DUAL_PORT", lpm_type => "altsyncram",
-						width_a => 8, widthad_a => 9, numwords_a => 512, outdata_reg_a => "UNREGISTERED",
-						width_b => 8, widthad_b => 9, numwords_b => 512, outdata_reg_b => "UNREGISTERED",
-						indata_reg_b => "CLOCK1",   address_reg_b => "CLOCK1", wrcontrol_wraddress_reg_b => "CLOCK1"
-		)
-		PORT MAP( 
-			clock0    => ClkA,		clock1    => ClkB,
-			wren_a    => WeA,		wren_b    => WeB,		
-			clocken0  => EnA,		clocken1  => EnB,
-			address_a => AddrA,		address_b => AddrB,					
-			data_a    => DiA,		data_b    => DiB,
-			q_a       => DoA,		q_b       => DoB
 			);
 
 end struct;
