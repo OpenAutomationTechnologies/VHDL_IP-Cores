aff5eb4404e2ce324b5818e88b7a62441a2e35b9
Jörg Zelenka <joerg.zelenka@br-automation.com>
2012-02-07 10:50:27 +0100

    [TASK] Change of directory structure and deleted obsolete files
    Changed directory structure according to "VHDL IP Task Force" (src, doc, tb). Deleted obsolete files
    (testbenches and templates). Added AXI_POWERLINK (not yet finished).

--- altera_release.bat
+++ altera_release.bat
@@ -7,17 +7,19 @@ mkdir release\altera_sopc\POWERLINK\doc
 mkdir release\altera_sopc\mif
 mkdir release\altera_sopc\POWERLINK\img
 
-copy active_hdl\src\mif\*.mif						release\altera_sopc\mif
-copy active_hdl\src\*.vhd							release\altera_sopc\POWERLINK\src
-copy active_hdl\src\lib\*.vhd						release\altera_sopc\POWERLINK\src\lib
-copy active_hdl\src\openMAC_DMAmaster\*.vhd			release\altera_sopc\POWERLINK\src\openMAC_DMAmaster
-copy active_hdl\compile\*.vhd						release\altera_sopc\POWERLINK\src
-copy active_hdl\src\altera_sopc\powerlink_hw.tcl	release\altera_sopc\POWERLINK
-copy active_hdl\src\altera_sopc\*.sdc				release\altera_sopc\POWERLINK\sdc
-copy documentation\*_Generic.pdf					release\altera_sopc\POWERLINK\doc
-copy documentation\*_Altera.pdf						release\altera_sopc\POWERLINK\doc
-copy documentation\OpenMAC.pdf						release\altera_sopc\POWERLINK\doc
-copy images\*.png							release\altera_sopc\POWERLINK\img
+copy active_hdl\src\mif\*.mif						        release\altera_sopc\mif
+copy active_hdl\src\openMAC\src\*.vhd				        release\altera_sopc\POWERLINK\src
+copy active_hdl\src\openMAC\src\openMAC_DMAmaster\*.vhd		release\altera_sopc\POWERLINK\src\openMAC_DMAmaster
+copy active_hdl\src\PDI\src\*.vhd				            release\altera_sopc\POWERLINK\src
+copy active_hdl\src\SPI\src\*.vhd				            release\altera_sopc\POWERLINK\src
+copy active_hdl\src\lib\src\*.vhd					        release\altera_sopc\POWERLINK\src\lib
+copy active_hdl\compile\*.vhd						        release\altera_sopc\POWERLINK\src
+copy altera_sopc\powerlink_hw.tcl	                        release\altera_sopc\POWERLINK
+copy altera_sopc\*.sdc				                        release\altera_sopc\POWERLINK\sdc
+copy documentation\*_Generic.pdf					        release\altera_sopc\POWERLINK\doc
+copy documentation\*_Altera.pdf						        release\altera_sopc\POWERLINK\doc
+copy documentation\OpenMAC.pdf						        release\altera_sopc\POWERLINK\doc
+copy images\*.png							                release\altera_sopc\POWERLINK\img
 
 del release\altera_sopc\POWERLINK\src\*_TB.vhd
 del release\altera_sopc\POWERLINK\src\*_Xilinx.vhd
