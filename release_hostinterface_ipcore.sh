3d3de761d570467c04b7c4ebc6f89e5e8d9fb4b4
Joerg Zelenka <joerg.zelenka@br-automation.com>
2013-12-13 08:23:31 +0100

    [TASK] Revise host interface ipcore documentation
    - Update doxyfile
    - Generate wavedrom files with ipcore release
    
    Change-Id: I149f5af20c596f4dfee44a30551793b3817fa7c9
    Reviewed-on: http://brateggevoat1/gerrit/1678
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Joerg Zelenka <joerg.zelenka@br-automation.com>

--- release_hostinterface_ipcore.sh
+++ release_hostinterface_ipcore.sh
@@ -10,42 +10,55 @@ then
 fi
 
 # create dir structure
-echo "create dir structure..."
-mkdir -p ${DIR_RELEASE}/altera/components
-mkdir -p ${DIR_RELEASE}/altera/components/img
-mkdir -p ${DIR_RELEASE}/altera/components/sdc
-mkdir -p ${DIR_RELEASE}/altera/components/tcl
-mkdir -p ${DIR_RELEASE}/altera/hostinterface/src
-mkdir -p ${DIR_RELEASE}/altera/lib/src
-mkdir -p ${DIR_RELEASE}/common/hostinterface/src
-mkdir -p ${DIR_RELEASE}/common/lib/src
+echo "create release dir..."
+mkdir -p ${DIR_RELEASE}
+
+# generate docs
+echo "generate docs..."
+DIR_DOC=common/hostinterface/doc
+pushd $DIR_DOC
+./create-this-doc --skip-doxygen
+popd
 
 # copy docs
-#echo "copy docs..."
-#
+echo "copy docs..."
+cp --parents ${DIR_DOC}/images/hostif-common_vs_dynamic_bridge.png  ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/images/hostif-example_sys_int.png           ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/images/hostif-dyn_bridge.png                ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/images/hostif-memmap.png                    ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/md/hostif.md                                ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/md/hostif_sc.md                             ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/hostif_mplx_rd-wr.svg              ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/hostif_rd-wr.svg                   ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/hostif_mplx_rd.svg                 ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/hostif_rd.svg                      ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/hostif_mplx_wr.svg                 ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/hostif_wr.svg                      ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/Doxyfile                                    ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/mainpage.txt                                ${DIR_RELEASE}
 
 # copy Altera host interface
 echo "copy altera powerlink ipcore..."
-cp altera/components/hostinterface_hw.tcl                   ${DIR_RELEASE}/altera/components
-cp altera/components/hostinterface_sw.tcl                   ${DIR_RELEASE}/altera/components
-cp altera/components/img/br.png                             ${DIR_RELEASE}/altera/components/img
-cp altera/components/sdc/hostinterface-async.sdc            ${DIR_RELEASE}/altera/components/sdc
-cp altera/components/tcl/hostinterface.tcl                  ${DIR_RELEASE}/altera/components/tcl
-cp altera/lib/src/dpRam-rtl-a.vhd                           ${DIR_RELEASE}/altera/lib/src
-cp common/lib/src/dpRam-e.vhd                               ${DIR_RELEASE}/common/lib/src
-cp common/lib/src/addrDecodeRtl.vhd                         ${DIR_RELEASE}/common/lib/src
-cp common/lib/src/binaryEncoderRtl.vhd                      ${DIR_RELEASE}/common/lib/src
-cp common/lib/src/cntRtl.vhd                                ${DIR_RELEASE}/common/lib/src
-cp common/lib/src/edgedetectorRtl.vhd                       ${DIR_RELEASE}/common/lib/src
-cp common/lib/src/lutFileRtl.vhd                            ${DIR_RELEASE}/common/lib/src
-cp common/lib/src/registerFileRtl.vhd                       ${DIR_RELEASE}/common/lib/src
-cp common/lib/src/synchronizerRtl.vhd                       ${DIR_RELEASE}/common/lib/src
-cp altera/hostinterface/src/alteraHostInterfaceRtl.vhd      ${DIR_RELEASE}/altera/hostinterface/src
-cp common/hostinterface/revision.txt                        ${DIR_RELEASE}/common/hostinterface
-cp common/hostinterface/src/hostInterfacePkg.vhd            ${DIR_RELEASE}/common/hostinterface/src
-cp common/hostinterface/src/hostInterfaceRtl.vhd            ${DIR_RELEASE}/common/hostinterface/src
-cp common/hostinterface/src/irqGenRtl.vhd                   ${DIR_RELEASE}/common/hostinterface/src
-cp common/hostinterface/src/dynamicBridgeRtl.vhd            ${DIR_RELEASE}/common/hostinterface/src
-cp common/hostinterface/src/statusControlRegRtl.vhd         ${DIR_RELEASE}/common/hostinterface/src
-cp common/hostinterface/src/parallelInterfaceRtl.vhd        ${DIR_RELEASE}/common/hostinterface/src
-cp common/lib/src/global.vhd                                ${DIR_RELEASE}/common/lib/src
+cp --parents altera/components/hostinterface_hw.tcl                 ${DIR_RELEASE}
+cp --parents altera/components/hostinterface_sw.tcl                 ${DIR_RELEASE}
+cp --parents altera/components/img/br.png                           ${DIR_RELEASE}
+cp --parents altera/components/sdc/hostinterface-async.sdc          ${DIR_RELEASE}
+cp --parents altera/components/tcl/hostinterface.tcl                ${DIR_RELEASE}
+cp --parents altera/lib/src/dpRam-rtl-a.vhd                         ${DIR_RELEASE}
+cp --parents common/lib/src/dpRam-e.vhd                             ${DIR_RELEASE}
+cp --parents common/lib/src/addrDecodeRtl.vhd                       ${DIR_RELEASE}
+cp --parents common/lib/src/binaryEncoderRtl.vhd                    ${DIR_RELEASE}
+cp --parents common/lib/src/cntRtl.vhd                              ${DIR_RELEASE}
+cp --parents common/lib/src/edgedetectorRtl.vhd                     ${DIR_RELEASE}
+cp --parents common/lib/src/lutFileRtl.vhd                          ${DIR_RELEASE}
+cp --parents common/lib/src/registerFileRtl.vhd                     ${DIR_RELEASE}
+cp --parents common/lib/src/synchronizerRtl.vhd                     ${DIR_RELEASE}
+cp --parents altera/hostinterface/src/alteraHostInterfaceRtl.vhd    ${DIR_RELEASE}
+cp --parents common/hostinterface/revision.txt                      ${DIR_RELEASE}
+cp --parents common/hostinterface/src/hostInterfacePkg.vhd          ${DIR_RELEASE}
+cp --parents common/hostinterface/src/hostInterfaceRtl.vhd          ${DIR_RELEASE}
+cp --parents common/hostinterface/src/irqGenRtl.vhd                 ${DIR_RELEASE}
+cp --parents common/hostinterface/src/dynamicBridgeRtl.vhd          ${DIR_RELEASE}
+cp --parents common/hostinterface/src/statusControlRegRtl.vhd       ${DIR_RELEASE}
+cp --parents common/hostinterface/src/parallelInterfaceRtl.vhd      ${DIR_RELEASE}
+cp --parents common/lib/src/global.vhd                              ${DIR_RELEASE}
