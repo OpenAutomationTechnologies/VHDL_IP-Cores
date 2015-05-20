5ff7eec77c8acb593ac4301d9e987d5afcc1020d
Joerg Zelenka <joerg.zelenka@br-automation.com>
2015-05-20 17:41:26 +0200

    [FIX] Fix openMAC descriptor DPRAM conflicts
    The openMAC IP-Core uses a DPRAM for simultaneous access to memory
    content. Depending on the timing it could happen that the same address
    is written and read from the ports at the same time. This could lead to
    an invalid read port value for one clock cycle, what confuses the
    openMAC descriptor handling logic.
    This fix ensures that port B can read valid data at any time reliably.
    - Create own entity for openMAC descriptor DPRAM.
    - Altera: Use read during write mode option to force old data read.
      This configuration is available for M4K, M9K and M10K resources.
    - Xilinx: Configure write port A to READ_FIRST mode according to
      Spartan 6 (ug383) and 7 series (ug473) FPGA devices.
    - Adapt openMAC to changed entity name.
    - Adapt openMAC component configuration (Qsys and XPS) and release
      script.
    - Add new DPRAM implementation to openMAC testbench.
    
    Change-Id: Ie888622a5db1291fcb3813949c86c4573e13ba82

--- release_openmac.sh
+++ release_openmac.sh
@@ -58,6 +58,7 @@ cp --parents altera/components/tcl/qsysUtil.tcl                 ${DIR_RELEASE}
 cp --parents common/util/tcl/ipcoreUtil.tcl                     ${DIR_RELEASE}
 cp --parents common/util/tcl/writeFile.tcl                      ${DIR_RELEASE}
 cp --parents common/memory/src/dpRam-e.vhd                      ${DIR_RELEASE}
+cp --parents common/memory/src/dpRamOpenmac-e.vhd               ${DIR_RELEASE}
 cp --parents common/memory/src/dpRamSplx-e.vhd                  ${DIR_RELEASE}
 cp --parents common/fifo/src/asyncFifo-e.vhd                    ${DIR_RELEASE}
 cp --parents common/openmac/tcl/openmac.tcl                     ${DIR_RELEASE}
@@ -74,6 +75,7 @@ cp --parents common/openmac/src/convRmiiToMii-rtl-ea.vhd        ${DIR_RELEASE}
 cp --parents common/openmac/src/openmac-rtl-ea.vhd              ${DIR_RELEASE}
 cp --parents common/openmac/src/openmacTop-rtl-ea.vhd           ${DIR_RELEASE}
 cp --parents altera/memory/src/dpRam-rtl-a.vhd                  ${DIR_RELEASE}
+cp --parents altera/memory/src/dpRamOpenmac-rtl-a.vhd           ${DIR_RELEASE}
 cp --parents altera/memory/src/dpRamSplx-rtl-a.vhd              ${DIR_RELEASE}
 cp --parents altera/fifo/src/asyncFifo-syn-a.vhd                ${DIR_RELEASE}
 cp --parents altera/openmac/src/alteraOpenmacTop-rtl-ea.vhd     ${DIR_RELEASE}
@@ -92,6 +94,7 @@ cp xilinx/components/pcores/axi_openmac_vX_YY_Z/data/axi_openmac_v2_1_0.mui  ${D
 
 cp --parents common/util/tcl/ipcoreUtil.tcl                     ${DIR_RELEASE}
 cp --parents common/memory/src/dpRam-e.vhd                      ${DIR_RELEASE}
+cp --parents common/memory/src/dpRamOpenmac-e.vhd               ${DIR_RELEASE}
 cp --parents common/memory/src/dpRamSplx-e.vhd                  ${DIR_RELEASE}
 cp --parents common/memory/src/dpRamSplxNbe-e.vhd               ${DIR_RELEASE}
 cp --parents common/fifo/src/asyncFifo-e.vhd                    ${DIR_RELEASE}
@@ -113,6 +116,7 @@ cp --parents common/openmac/src/mmSlaveConv-rtl-ea.vhd          ${DIR_RELEASE}
 cp --parents common/openmac/src/openmac-rtl-ea.vhd              ${DIR_RELEASE}
 cp --parents common/openmac/src/openmacTop-rtl-ea.vhd           ${DIR_RELEASE}
 cp --parents xilinx/memory/src/dpRam-rtl-a.vhd                  ${DIR_RELEASE}
+cp --parents xilinx/memory/src/dpRamOpenmac-rtl-a.vhd           ${DIR_RELEASE}
 cp --parents xilinx/memory/src/dpRamSplx-rtl-a.vhd              ${DIR_RELEASE}
 cp --parents xilinx/memory/src/dpRamSplxNbe-rtl-a.vhd           ${DIR_RELEASE}
 cp --parents xilinx/openmac/src/ipifMasterHandler-rtl-ea.vhd    ${DIR_RELEASE}
