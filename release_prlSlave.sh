9224b665de5963de6e074ab59e046586bb170e3b
Joerg Zelenka <joerg.zelenka@br-automation.com>
2015-12-03 10:35:46 +0100

    [FEATURE] Add documentation for parallel interface ipcore
    - Add doxygen documentation for prlSlave ipcore
    - Update timing documentation
    - Update the ipcore release script
    
    Change-Id: Ic81aadcb2d3d08775a6d09e3e3cb28826c499762

--- release_prlSlave.sh
+++ release_prlSlave.sh
@@ -2,6 +2,7 @@
 
 DIR_RELEASE=$1
 IP_VERSION=$2
+DIR_DOC=doc/parallelinterface
 
 if [ -z "${DIR_RELEASE}" ];
 then
@@ -17,6 +18,30 @@ fi
 echo "create release dir..."
 mkdir -p ${DIR_RELEASE}
 
+# generate docs
+echo "generate docs..."
+pushd $DIR_DOC
+./create-this-doc --skip-doxygen
+popd
+
+# copy docs
+echo "copy docs..."
+cp --parents ${DIR_DOC}/wavedrom/parallelif_mplx_rd.svg         ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/parallelif_mplx_wr.svg         ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/parallelif_rd.svg              ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/wavedrom/parallelif_wr.svg              ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/md/parallelif.md                        ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/doxyfile                                ${DIR_RELEASE}
+cp --parents ${DIR_DOC}/mainpage.txt                            ${DIR_RELEASE}
+
+# create revision.txt
+REV_FILE=${DIR_RELEASE}/${DIR_DOC}/revision.md
+echo "Revision {#revision}" > $REV_FILE
+echo "========" >> $REV_FILE
+echo "" >> $REV_FILE
+git log --format="- %s" -- */parallelinterface/* >> $REV_FILE
+
+# Copy sources
 cp --parents common/latch/src/dataLatch-e.vhd                   ${DIR_RELEASE}
 cp --parents altera/latch/src/dataLatch-syn-a.vhd               ${DIR_RELEASE}
 cp --parents common/parallelinterface/src/prlSlave-rtl-ea.vhd   ${DIR_RELEASE}
